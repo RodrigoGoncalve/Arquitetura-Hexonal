b45d5e363e3da132df70364b8583b0fb24f90442
Rodrigo Goncalves <ro.goncalve@gmail.com>
2024-09-02 20:31:03 -0300

    Criando controller pra update

--- src/main/java/com/arames/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/arames/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,8 +3,10 @@ package com.arames.hexagonal.adapters.in.controller;
 import com.arames.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.arames.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.arames.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.arames.hexagonal.application.core.domain.Customer;
 import com.arames.hexagonal.application.ports.in.FindCustomerByIdImputPort;
 import com.arames.hexagonal.application.ports.in.InsertCustomerInputPort;
+import com.arames.hexagonal.application.ports.in.UpdateCustomerImputPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +26,9 @@ public class CustomerController {
     @Autowired
     private CustomerMapper customerMapper;
 
+    @Autowired
+    private UpdateCustomerImputPort updateCustomerImputPort;
+
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest){
         var customer = customerMapper.toCustomer(customerRequest);
@@ -36,6 +41,14 @@ public class CustomerController {
         var customer = findCustomerByIdImputPort.find(id);
         var customerResponse = customerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok().body(customerResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id,@Valid @RequestBody CustomerRequest customerRequest){
+        Customer customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerImputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/arames/hexagonal/application/core/usecase/UpdateCustomerUserCase.java
+++ src/main/java/com/arames/hexagonal/application/core/usecase/UpdateCustomerUserCase.java
@@ -2,10 +2,11 @@ package com.arames.hexagonal.application.core.usecase;
 
 import com.arames.hexagonal.application.core.domain.Customer;
 import com.arames.hexagonal.application.ports.in.FindCustomerByIdImputPort;
+import com.arames.hexagonal.application.ports.in.UpdateCustomerImputPort;
 import com.arames.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
 import com.arames.hexagonal.application.ports.out.UpdateCustomerOutputPort;
 
-public class UpdateCustomerUserCase {
+public class UpdateCustomerUserCase implements UpdateCustomerImputPort {
 
     private final FindCustomerByIdImputPort findCustomerByIdImputPort;
     private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
@@ -19,6 +20,7 @@ public class UpdateCustomerUserCase {
         this.updateCustomerOutputPort = updateCustomerOutputPort;
     }
 
+    @Override
     public void update(Customer customer, String zipCode){
         findCustomerByIdImputPort.find(customer.getId());
         var address = findAddressByZipCodeOutputPort.find(zipCode);
